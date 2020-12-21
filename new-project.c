0a9db01bb203b06dd196ba93fa06ed981e3ae9fd
nhatlinh4459 <64762945+nhatlinh4459@users.noreply.github.com>
2020-12-21 16:53:13 +0700

    work assign

--- new-project.c
+++ new-project.c
@@ -5,6 +5,12 @@
 #include <string.h>
 
 #define CITY_COUNT_INITIAL 312 
+#define CITY_NAME_LENGTH 25
+#define CITY_LIST_FILE
+#define FLIGHT_LIST_FILE
+#define DISTANCE_LIST_FILE
+#define CAR_SPEED 1
+#define PLANE_SPEED 100
 
 typedef struct {
 	int cityCount;
@@ -13,30 +19,31 @@ typedef struct {
     int** distance;
     int** connections;
 } Graph;
-// main skeleton: 
+// linh:
 Graph createGraph(int cityCount);
+void readGraphFromFile(Graph graph);
 void dropGraph(Graph graph);
+// make change to text file also
 void addFlight(Graph graph, char * source, char * destination);
 void removeFLight(Graph graph, char * source, char * destination);
 
+// hai:
+// generate flight file
+void printShortestRoute(Graph graph, char* source, char* destination);
 void printAllFlight(Graph graph);
 void printAllFlightFromCity(Graph graph, char * city);
 
-void printQuickestRoute(Graph graph, char* source, char* destination);
-//shortest patht algorithm on connections graph
-void printShortestRoute(Graph graph, char* source, char* destination);
-//quickest route is always by car, no need to run algorithm here
-
+// khoi:
 int getCityID(Graph graph, char * city);
 char * getCityName(Graph graph, int cityID);
-
+void sortDistance(Graph Graph, int * output, int size);
+// text input preprocess
 void MENU_USER(){
 	printf("\n\tUS & Canada cities flight suggestion system\n");
 	printf("---------------------------------\n\n");
 	printf("1. PRINT ALL FLIGHTS\n\n");
 	printf("2. PRINT ALL FLIGHT FROM A CITY\n\n");
 	printf("3. FIND SHORTEST TRAVEL ROUTE BETWEEN TWO CITIES\n\n");
-	printf("4. FIND QUICKEST TRAVEL ROUTE BETWEEN TWO CITIES\n\n");
 	printf("---------------------------------\n\n");
 }
 void MENU_ADMINISTRATOR(){
@@ -45,7 +52,7 @@ void MENU_ADMINISTRATOR(){
 	printf("1. PRINT ALL FLIGHTS\n\n");
 	printf("2. PRINT ALL FLIGHT FROM A CITY\n\n");
 	printf("3. SORT ALL DISTANCES BETWEEN TWO CITIES\n\n");
-	printf("4. MODIFY MAP: ADD FLIGHT\n\n");
+	printf("4. MODIFY MAP: ADD/REMOVE FLIGHT\n\n");
 	printf("---------------------------------\n\n");
 }
 	
